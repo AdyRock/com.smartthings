24be3eb1146cc97091bacd6d6d1024fe301482c7
AdyRock <38184396+AdyRock@users.noreply.github.com>
2021-09-26 11:22:08 +0100

    Tidy up

--- api.js
+++ api.js
@@ -1,12 +1,6 @@
 /*jslint node: true */
 
 module.exports = {
-    async getRoot( { homey, query } )
-    {
-        //const result = await Homey.app.getSomething( args );
-        console.log( "Get: ", query );
-        return "OK";
-    },
     async getLog( { homey, query } )
     {
         return homey.app.diagLog;
@@ -14,11 +8,6 @@ module.exports = {
     async getDetect( { homey, query } )
     {
         return homey.app.detectedDevices;
-    },
-    async getWebHook( { homey, query } )
-    {
-        console.log( "Get: ", args );
-        return "OK";
     },
     async clearLog( { homey, body } )
     {
@@ -36,39 +25,4 @@ module.exports = {
 
         throw new Error( "No Response" );
     },
-    async sendRoot( { homey, body } )
-    {
-        console.log( "Post: ", args );
-        return homey.app.addSomething( args );
-    },
-    async sendWebHook( { homey, body } )
-    {
-        console.log( "Post: ", args );
-        //return homey.app.addSomething( args );
-
-        var response = "";
-        if ( body.pingData && body.pingData.challenge )
-        {
-            response = {
-                "pingData":
-                {
-                    "challenge": body.pingData.challenge
-                }
-
-            };
-        }
-        const result = response;
-        console.log( "Post Reply: ", response );
-        return result;
-    },
-    async putWebHook( { homey, body } )
-    {
-        console.log( "Put: ", args );
-        return homey.app.updateSomething( args );
-    },
-    async deleteWebHook( { homey, body } )
-    {
-        console.log( "Delete: ", args );
-        return homey.app.deleteSomething( args );
-    }
 };
